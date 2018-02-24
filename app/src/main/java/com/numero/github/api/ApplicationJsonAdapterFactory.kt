2074f9231a0d9e18f3c74fcf50d0ea965adec5a1
NUmeroAndDev <numero.dev.and@gmail.com>
2018-02-24 22:22:08 +0900

    ApiModuleの追加

---
+++ app/src/main/java/com/numero/github/api/ApplicationJsonAdapterFactory.kt
@@ -0,0 +1,11 @@
+package com.numero.github.api
+
+import com.squareup.moshi.JsonAdapter
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+@KotshiJsonAdapterFactory
+abstract class ApplicationJsonAdapterFactory : JsonAdapter.Factory {
+    companion object {
+        val INSTANCE: ApplicationJsonAdapterFactory = KotshiApplicationJsonAdapterFactory()
+    }
+}
---
+++ app/src/main/java/com/numero/github/di/ApiModule.kt
@@ -0,0 +1,46 @@
+package com.numero.github.di
+
+import com.numero.github.BuildConfig
+import com.numero.github.api.ApplicationJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            readTimeout(15, TimeUnit.SECONDS)
+            writeTimeout(15, TimeUnit.SECONDS)
+            if (BuildConfig.DEBUG) {
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+            }
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().apply {
+                    add(ApplicationJsonAdapterFactory.INSTANCE)
+                }.build()))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+    }
+}
