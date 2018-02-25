549ffd0545444268dbd0292fff6947c3b3b73e81
NUmeroAndDev <numero.dev.and@gmail.com>
2018-02-25 13:13:11 +0900

    LoginFragmentの追加

--- app/src/main/java/com/numero/github/activity/LoginActivity.kt
+++ app/src/main/java/com/numero/github/activity/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.numero.github.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.numero.github.R
+import com.numero.github.fragment.LoginFragment
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -12,5 +13,14 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
 
+        showLoginFragment()
+    }
+
+    private fun showLoginFragment() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.container) as? LoginFragment ?:
+        LoginFragment.newInstance().also {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, it).commit()
+        }
     }
 }
---
+++ app/src/main/java/com/numero/github/fragment/LoginFragment.kt
@@ -0,0 +1,32 @@
+package com.numero.github.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.numero.github.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    companion object {
+        fun newInstance(): LoginFragment = LoginFragment()
+    }
+}
