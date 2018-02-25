b9a810f9246c29a7614265120738deefe672124a
NUmeroAndDev <numero.dev.and@gmail.com>
2018-02-25 13:35:33 +0900

    ログイン処理の追加

--- app/src/main/java/com/numero/github/activity/LoginActivity.kt
+++ app/src/main/java/com/numero/github/activity/LoginActivity.kt
@@ -2,17 +2,29 @@ package com.numero.github.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.numero.github.GithubApplication
 import com.numero.github.R
+import com.numero.github.api.GithubApi
 import com.numero.github.fragment.LoginFragment
 import com.numero.github.presenter.LoginPresenter
 import kotlinx.android.synthetic.main.activity_login.*
+import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var githubApi: GithubApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
+
+        application?.apply {
+            if (this is GithubApplication) {
+                this.component.inject(this@LoginActivity)
+            }
+        }
 
         showLoginFragment()
     }
@@ -23,6 +35,6 @@ class LoginActivity : AppCompatActivity() {
                     supportFragmentManager.beginTransaction()
                             .replace(R.id.container, it).commit()
                 }
-        LoginPresenter(fragment)
+        LoginPresenter(fragment, githubApi)
     }
 }
--- app/src/main/java/com/numero/github/presenter/LoginPresenter.kt
+++ app/src/main/java/com/numero/github/presenter/LoginPresenter.kt
@@ -1,9 +1,15 @@
 package com.numero.github.presenter
 
+import android.util.Base64
+import com.numero.github.BuildConfig
+import com.numero.github.api.GithubApi
+import com.numero.github.api.request.AuthParams
 import com.numero.github.contract.LoginContract
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
-class LoginPresenter(private val view: LoginContract.View) : LoginContract.Presenter {
+class LoginPresenter(private val view: LoginContract.View, private val githubApi: GithubApi) : LoginContract.Presenter {
 
     private var disposable: Disposable? = null
 
@@ -23,6 +29,18 @@ class LoginPresenter(private val view: LoginContract.View) : LoginContract.Prese
     }
 
     override fun login(id: String, password: String) {
-
+        view.showProgress()
+        val params = AuthParams(BuildConfig.APPLICATION_ID + "_Android_${System.currentTimeMillis()}")
+        val authorization = "Basic " + Base64.encodeToString((id + ":" + password).toByteArray(), Base64.NO_WRAP)
+        disposable = githubApi.login(params, authorization)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    view.hideProgress()
+                    view.successLogin()
+                }, {
+                    view.hideProgress()
+                    it.printStackTrace()
+                })
     }
 }
