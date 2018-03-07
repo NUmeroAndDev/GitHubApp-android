2d954c85ab2bb0c549e72d1ec77f6037af48031d
NUmeroAndDev <numero.dev.and@gmail.com>
2018-03-07 20:40:22 +0900

    戻るボタンの追加

--- app/src/main/java/com/numero/github/activity/RepositoryActivity.kt
+++ app/src/main/java/com/numero/github/activity/RepositoryActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.numero.github.R
 import com.numero.github.extension.component
 import com.numero.github.extension.replaceFragment
@@ -30,9 +31,23 @@ class RepositoryActivity : AppCompatActivity(),
         setContentView(R.layout.activity_repository)
         setSupportActionBar(toolbar)
 
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+
         component?.inject(this)
 
         showContentListFragment(repositoryName)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClickContent(content: Content) {
