039d5f0eb60adaf4f26a99824c2f598ca4327e1e
NUmeroAndDev <numero.dev.and@gmail.com>
2018-03-10 14:02:22 +0900

    Repository用のFragmentを追加

---
+++ app/src/main/java/com/numero/github/fragment/RepositoryDetailFragment.kt
@@ -0,0 +1,45 @@
+package com.numero.github.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.numero.github.R
+
+class RepositoryDetailFragment : Fragment() {
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_repository_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun initViews() {
+
+    }
+
+    companion object {
+        fun newInstance(): RepositoryDetailFragment = RepositoryDetailFragment()
+    }
+}
--- app/src/main/java/com/numero/github/view/RepositoryStatusView.kt
+++ app/src/main/java/com/numero/github/view/RepositoryStatusView.kt
@@ -5,11 +5,29 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.numero.github.R
+import kotlinx.android.synthetic.main.view_repository_status.view.*
 
 class RepositoryStatusView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    var unwatchCount: Int = 0
+        set(value) {
+            field = value
+            unwatchTextView.text = value.toString()
+        }
+
+    var starCount: Int = 0
+        set(value) {
+            field = value
+            starTextView.text = value.toString()
+        }
+
+    var forkCount = 0
+        set(value) {
+            field = value
+            forkTextView.text = value.toString()
+        }
 
     init {
         View.inflate(context, R.layout.view_repository_status, this)
     }
-
 }
